bafeb8288be482c8005884e76e3b63710812c1ce
gaffeyQiu <253896514@qq.com>
2022-02-28 22:06:19 +0800

    feta: create course

--- web/src/bin/server1.rs
+++ web/src/bin/server1.rs
@@ -1,18 +1,18 @@
 use std::io;
 
-use actix_web::{web, Responder, HttpResponse, App, HttpServer};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 // 配置 route
 pub fn general_routes(cfg: &mut web::ServiceConfig) {
-	cfg.route("/health", web::get().to(health_check_handler));
+    cfg.route("/health", web::get().to(health_check_handler));
 }
 
 // 配置 handler
 pub async fn health_check_handler() -> impl Responder {
-	HttpResponse::Ok().json("Actix Web Service is Running!")
+    HttpResponse::Ok().json("Actix Web Service is Running!")
 }
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
-	let app = move || App::new().configure(general_routes);
-	HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
+    let app = move || App::new().configure(general_routes);
+    HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
 }
--- web/src/bin/teacher-service.rs
+++ web/src/bin/teacher-service.rs
@@ -1,12 +1,15 @@
 use std::sync::Mutex;
 
-use actix_web::{App, HttpServer, web};
+use actix_web::{web, App, HttpServer};
+use routers::course_routes;
 use routers::general_routes;
 use state::AppState;
 use std::io;
 
 #[path = "../handlers.rs"]
 mod handlers;
+#[path = "../models.rs"]
+mod models;
 #[path = "../routers.rs"]
 mod routers;
 #[path = "../state.rs"]
@@ -14,14 +17,18 @@ mod state;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
-	let shared_data = web::Data::new(AppState {
-		health_check_response: "I'm OK".to_string(),
-		visit_count: Mutex::new(0),
-	});
+    let shared_data = web::Data::new(AppState {
+        health_check_response: "I'm OK".to_string(),
+        visit_count: Mutex::new(0),
+        courses: Mutex::new(vec![]),
+    });
 
-	let app = move || {
-		App::new().app_data(shared_data.clone()).configure(general_routes)
-	};
+    let app = move || {
+        App::new()
+            .app_data(shared_data.clone())
+            .configure(general_routes)
+            .configure(course_routes)
+    };
 
-	HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
-}	
+    HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
+}
